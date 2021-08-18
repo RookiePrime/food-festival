710b5635bef96cc692c240b6ea13323a4feb1f16
Kiefer Levine <kiefer_8@hotmail.com>
2021-08-18 18:45:15 -0400

    Implemented webpacked scripts

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const webpack = require('webpack');
+
+module.exports = {
+    entry: './assets/js/script.js',
+    output: {
+        path: __dirname + '/dist',
+        filename: 'main.bundle.js'
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+    ],
+    mode: 'development'
+}
